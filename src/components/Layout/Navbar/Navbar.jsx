0a63f84e45841b8489e7b568c78275e40280e3a9
arai5563 <91533123+arai5563@users.noreply.github.com>
2022-02-26 21:39:43 +0530

    Navbar Search

--- src/components/Layout/Navbar/Navbar.jsx
+++ src/components/Layout/Navbar/Navbar.jsx
@@ -1,33 +1,59 @@
-import React from "react";
+import React, { useCallback } from "react";
 import "./Navbar.css";
+import { useState } from "react";
 
-import {IoIosMenu} from 'react-icons/io';
-import {BsSearch} from 'react-icons/bs';
-import {BsFillBookmarkPlusFill} from 'react-icons/bs';
-import {BsFillCaretDownFill} from 'react-icons/bs';
+import { IoIosMenu } from 'react-icons/io';
+import { BsSearch } from 'react-icons/bs';
+import { BsFillBookmarkPlusFill } from 'react-icons/bs';
+import { BsFillCaretDownFill } from 'react-icons/bs';
 
 export function Navbar() {
 
     const input_text = {
-        height:"28px",
-        width:"600px",
-        borderTopRightRadius:"8px",
-        borderBottomRightRadius:"8px",
+        height: "28px",
+        width: "600px",
+        borderTopRightRadius: "8px",
+        borderBottomRightRadius: "8px",
         marginLeft: "-1.5px",
         marginBottom: "5px",
-        padding:"-2px"
-    } ;
+        padding: "-2px"
+    };
 
     const select = {
-        borderTopLeftRadius:"7px",
-        borderBottomLeftRadius:"7px",
-        height:"33px",
-        width:"55px"
+        borderTopLeftRadius: "7px",
+        borderBottomLeftRadius: "7px",
+        height: "33px",
+        width: "55px"
     }
 
-    return(
+    const [search, setSearch] = useState([]);
+
+    const debounce = (func) => {
+        let timer;
+        return function (...args){
+            const context = this;
+
+            if(timer) clearTimeout(timer)
+            timer = setTimeout ( () => {
+                timer = null;
+                func.apply(context,args);
+            },500);
+        }
+    }
+
+    const handleChange = (event) => {
+        const { value } = event.target;
+        fetch(`https://imdb-api.com/en/API/Search/k_7ba6x7la/${value}`)
+            .then(res => res.json())
+            .then(json => setSearch(json.results));
+    };
+
+    const optimiseVersion = useCallback(debounce(handleChange));
+
+    return (
         <div>
-            
+
+
             <div id="nav_box">
                 <div id="nav_logo" >
                     <img src="https://variety.com/wp-content/uploads/2017/02/imdb1.png?w=681&h=383&crop=1" alt="" />
@@ -37,13 +63,13 @@ export function Navbar() {
                 <div id="nav_menu_box">
 
                     <div>
-                    <IoIosMenu size={27} color={"grey"}/>
+                        <IoIosMenu size={27} color={"grey"} />
                     </div>
 
                     <div id="nav_menu">
-                    <strong> Menu </strong>
+                        <strong> Menu </strong>
                     </div>
-                 
+
                 </div>
 
 
@@ -54,11 +80,11 @@ export function Navbar() {
                     </div>
 
                     <div>
-                        <input style={input_text} type="text" placeholder="Search IMDb"/>
+                        <input style={input_text} type="text" onChange={optimiseVersion} placeholder="Search IMDb" />
                     </div>
 
                     <div id="input_search">
-                       <a href=""> <BsSearch color={"grey"}/> </a>
+                        <a href=""> <BsSearch color={"grey"} /> </a>
                     </div>
                 </div>
 
@@ -69,40 +95,62 @@ export function Navbar() {
 
 
                 <div id="dummy_line">
-                    
+
                 </div>
 
 
-             <a href="">   <div id="nav_watchlist_box">
+                <a href="">   <div id="nav_watchlist_box">
                     <div>
-                        <BsFillBookmarkPlusFill color={"grey"} size={20}/>
+                        <BsFillBookmarkPlusFill color={"grey"} size={20} />
                     </div>
                     <div id="nav_watchlist">
-                          Watchlist
+                        Watchlist
                     </div>
                 </div> </a>
 
 
                 <div id="nav_login">
-                   <strong> Sign In </strong> 
+                    <strong> Sign In </strong>
                 </div>
 
 
                 <div id="nav_lang_box">
                     <div id="nav_lang">
-                       <strong> EN </strong> 
+                        <strong> EN </strong>
                     </div>
 
-                    <div style={{cursor:'pointer'}}>
+                    <div style={{ cursor: 'pointer' }}>
                         <BsFillCaretDownFill color={"white"} />
                     </div>
                 </div>
 
 
-              
+
             </div>
-           
-           
+
+            <div id="input-result">
+
+                {search?.length > 0 &&
+                    <div className={`autocomplete`}>
+                        {search?.map((el, i) =>
+                            <div key={i} className={`autocompleteItems`}>
+                                <div className={`result-div-main`}>
+                                    <div id={`result-div2`}><img src= {el.image} alt="" /></div>
+                                    <div id={`result-div1`}>{el.title} <br/>
+                                              {el.description}
+                                    </div>
+                                   
+                                </div>
+                            </div>
+                        )}
+                        
+    
+                    </div>
+                }
+
+            </div>
+
+
         </div>
     );
 }
